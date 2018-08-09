c67ebed3e49f03668eaef584f3099280c791d349
Light-Moon <1424686850@qq.com>
2018-08-09 23:42:04 +0800

    added two classes

---
+++ src/main/java/com/ctg/jmemadmin/resources/MemcachedService.java
@@ -0,0 +1,59 @@
+package com.ctg.jmemadmin.resources;
+
+import javax.inject.Singleton;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//TODO:只构思个框架，待细化填充
+@Singleton
+@Path("/memcached")
+public class MemcachedService {
+	private static final Logger LOG = LoggerFactory.getLogger(MemcachedService.class);
+	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	public Object startMemcachedInstance() {
+		
+		return null;
+	}
+	
+	@POST
+	public Object stopMemcachedInstance() {
+		
+		return null;
+	}
+	
+	@POST
+	public Object set() {
+		
+		return null;
+	}
+	
+	@GET
+	public Object get() {
+		
+		return null;
+	}
+	
+	public Object delete() {
+		
+		return null;
+	}
+	
+	public Object flushAll() {
+		
+		return null;
+	}
+	
+	@GET
+	public Object listKeys() {
+		
+		return null;
+	}
+}
---
+++ src/main/java/com/ctg/jmemadmin/resources/SystemStatusService.java
@@ -0,0 +1,22 @@
+package com.ctg.jmemadmin.resources;
+
+import javax.inject.Singleton;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//TODO:只构思个框架，待细化填充
+
+@Singleton
+@Path("/status")
+public class SystemStatusService {
+	private static final Logger LOG = LoggerFactory.getLogger(SystemStatusService.class);
+	
+	@GET
+	public Object stats() {
+		
+		return null;
+	}
+}
