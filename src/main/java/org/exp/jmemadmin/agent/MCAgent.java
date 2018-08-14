c25d20306e2db177f637bd56be80c09ef03ceb83
陈金曈 <chenjt@chinatelecom.cn>
2018-08-14 17:31:38 +0800

    Moved agent class into package agent.

---
+++ src/main/java/org/exp/jmemadmin/agent/MCAgent.java
@@ -0,0 +1,25 @@
+package org.exp.jmemadmin.agent;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MCAgent implements Agent {
+    private static final Logger LOG = LoggerFactory.getLogger(MCAgent.class);
+
+    public MCAgent() {
+
+    }
+
+    @Override
+    public void start() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void stop() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ src/main/java/org/exp/jmemadmin/monitor/MCMonitor.java
@@ -0,0 +1,11 @@
+package org.exp.jmemadmin.monitor;
+
+public class MCMonitor extends Monitor {
+
+    @Override
+    protected void work() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
