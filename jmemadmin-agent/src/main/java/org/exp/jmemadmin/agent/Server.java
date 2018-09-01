47706a7ce1d9909ab8220fbe2e40bb47f436bc77
陈金曈 <chenjt@chinatelecom.cn>
2018-09-01 15:59:16 +0800

    Added agent server codes.

--- jmemadmin-agent/src/main/java/org/exp/jmemadmin/agent/Server.java
+++
@@ -1,5 +0,0 @@
-package org.exp.jmemadmin.agent;
-
-public class Server {
-
-}
---
+++ jmemadmin-agent/src/main/java/org/exp/jmemadmin/agent/server/AgentServer.java
@@ -0,0 +1,84 @@
+package org.exp.jmemadmin.agent.server;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.URISyntaxException;
+import java.net.UnknownHostException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.exp.jmemadmin.agent.services.MCAgentService;
+import org.exp.jmemadmin.common.Constants;
+import org.exp.jmemadmin.common.utils.HTTPUtils;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.logging.LoggingFeature;
+import org.glassfish.jersey.moxy.json.MoxyJsonFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class AgentServer {
+    private static final Log LOG = LogFactory.getLog(AgentServer.class);
+
+    private String hostAddress = null;
+
+    // TODO Agent server port.
+    private int port;
+
+    private HttpServer httpServer = null;
+
+    public AgentServer() {
+        init();
+    }
+
+    public AgentServer(int port) {
+        this.port = port;
+        init();
+    }
+
+    private void init() {
+        try {
+            this.hostAddress = InetAddress.getLocalHost().getHostAddress();
+            ResourceConfig config = new ResourceConfig();
+            config.packages(false, MCAgentService.class.getPackage().getName());
+            config.register(MoxyJsonFeature.class);
+            config.register(LoggingFeature.class).property(LoggingFeature.LOGGING_FEATURE_LOGGER_LEVEL_SERVER, "INFO");
+            this.httpServer = GrizzlyHttpServerFactory.createHttpServer(HTTPUtils.buildURI(getHostAddress(), getPort()), config, false, null, false);
+            LOG.info("REST server initialized, address is [" + getHostAddress() + Constants.COLON_DELIMITER + getPort() + "].");
+        } catch (UnknownHostException | URISyntaxException e) {
+            LOG.error(e.getMessage(), e);
+        }
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public String getHostAddress() {
+        return hostAddress;
+    }
+
+    /**
+     * Start server.
+     *
+     * @throws IOException
+     */
+    public void start() throws IOException {
+        if (null == httpServer) {
+            throw new NullPointerException("No such server.");
+        }
+        if (httpServer.isStarted()) {
+            throw new IllegalStateException("Server is still running.");
+        }
+        httpServer.start();
+    }
+
+    public static void main(String[] args) {
+        AgentServer server = 0 == args.length ? new AgentServer() : new AgentServer(Integer.parseInt(args[0]));
+        try {
+            server.start();
+        } catch (IOException e) {
+            LOG.error(e.getMessage(), e);
+        }
+    }
+
+}
