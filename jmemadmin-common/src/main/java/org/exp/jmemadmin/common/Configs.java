63062698a799380698304fa26399bec74143256e
Light-Moon <1424686850@qq.com>
2018-10-16 14:25:56 +0800

    Added ServerConfig class and configuration file.

--- jmemadmin-common/src/main/java/org/exp/jmemadmin/common/Configs.java
+++ jmemadmin-common/src/main/java/org/exp/jmemadmin/common/Configs.java
@@ -84,45 +84,6 @@ public class Configs {
     private static final String MC_STARTUP_CONFIGURABLE_PARAMETERS_KEY = "jmemadmin.mc.startup.configurable.parameters";
     private static final String DEFAULT_MC_STARTUP_CONFIGURABLE_PARAMETERS_VALUE = " -d -u root -c 1000 ";
 
-    /*
-     * SockIOPool Configuration Items
-     */
-    private static final String SOCKIOPOOL_FAILOVER_KEY = "jmemadmin.sockiopool.failover";
-    private static final boolean DEFAULT_SOCKIOPOOL_FAILOVER_VALUE = true;
-
-    private static final String SOCKIOPOOL_INIT_CONNECTIONS_KEY = "jmemadmin.sockiopool.init.connections";
-    private static final int DEFAULT_SOCKIOPOOL_INIT_CONNECTIONS_VALUE = 10;
-
-    private static final String SOCKIOPOOL_MIN_CONNECTIONS_KEY = "jmemadmin.sockiopool.min.connections";
-    private static final int DEFAULT_SOCKIOPOOL_MIN_CONNECTIONS_VALUE = 5;
-
-    private static final String SOCKIOPOOL_MAX_CONNECTIONS_KEY = "jmemadmin.sockiopool.max.connections";
-    private static final int DEFAULT_SOCKIOPOOL_MAX_CONNECTIONS_VALUE = 250;
-
-    private static final String SOCKIOPOOL_MAINT_SLEEP_KEY = "jmemadmin.sockiopool.maint.sleep";
-    private static final int DEFAULT_SOCKIOPOOL_MAINT_SLEEP_VALUE = 30;
-
-    private static final String SOCKIOPOOL_NAGLE_KEY = "jmemadmin.sockiopool.nagle";
-    private static final boolean DEFAULT_SOCKIOPOOL_NAGLE_VALUE = false;
-
-    private static final String SOCKIOPOOL_SOCKET_TIMEOUT_KEY = "jmemadmin.sockiopool.socket.timeout";
-    private static final int DEFAULT_SOCKIOPOOL_SOCKET_TIMEOUT_VALUE = 3000;
-
-    private static final String SOCKIOPOOL_ALIVE_CHECK_KEY = "jmemadmin.sockiopool.alive.check";
-    private static final boolean DEFAULT_SOCKIOPOOL_ALIVE_CHECK_VALUE = true;
-
-    private static final String SOCKIOPOOL_MAX_IDLE_KEY = "jmemadmin.sockiopool.max.idle";
-    private static final int DEFAULT_SOCKIOPOOL_MAX_IDLE_VALUE = 1000 * 30 * 30;
-
-    private static final String SOCKIOPOOL_CONNECT_TIMEOUT_KEY = "jmemadmin.sockiopool.connect.timeout";
-    private static final int DEFAULT_SOCKIOPOOL_CONNECT_TIMEOUT_VALUE = 0;
-
-    private static final String SOCKIOPOOL_MEMNAME_PREFIX_KEY = "jmemadmin.sockiopool.memname.prefix";
-    private static final String DEFAULT_SOCKIOPOOL_MEMNAME_PREFIX_VALUE = "mem";
-
-    private static final String SOCKIOPOOL_GARBAGE_COLLECTION_INTERVAL_MS_KEY = "jmemadmin.sockiopool.garbage.collection.interval.ms";
-    private static final int DEFAULT_SOCKIOPOOL_GARBAGE_COLLECTION_INTERVAL_MS_VALUE = 1000000;
-
     private static final Configuration CONF = new Configuration();
 
     static {
@@ -213,51 +174,4 @@ public class Configs {
         return getConf().getTrimmed(MC_STARTUP_CONFIGURABLE_PARAMETERS_KEY, DEFAULT_MC_STARTUP_CONFIGURABLE_PARAMETERS_VALUE);
     }
 
-    public static boolean getPoolFailover() {
-        return getConf().getBoolean(SOCKIOPOOL_FAILOVER_KEY, DEFAULT_SOCKIOPOOL_FAILOVER_VALUE);
-    }
-
-    public static int getPoolInitConns() {
-        return getConf().getInt(SOCKIOPOOL_INIT_CONNECTIONS_KEY, DEFAULT_SOCKIOPOOL_INIT_CONNECTIONS_VALUE);
-    }
-
-    public static int getPoolMinConns() {
-        return getConf().getInt(SOCKIOPOOL_MIN_CONNECTIONS_KEY, DEFAULT_SOCKIOPOOL_MIN_CONNECTIONS_VALUE);
-    }
-
-    public static int getPoolMaxConns() {
-        return getConf().getInt(SOCKIOPOOL_MAX_CONNECTIONS_KEY, DEFAULT_SOCKIOPOOL_MAX_CONNECTIONS_VALUE);
-    }
-
-    public static int getPoolMaintSleep() {
-        return getConf().getInt(SOCKIOPOOL_MAINT_SLEEP_KEY, DEFAULT_SOCKIOPOOL_MAINT_SLEEP_VALUE);
-    }
-
-    public static boolean getPoolNagle() {
-        return getConf().getBoolean(SOCKIOPOOL_NAGLE_KEY, DEFAULT_SOCKIOPOOL_NAGLE_VALUE);
-    }
-
-    public static int getPoolSocketTimeout() {
-        return getConf().getInt(SOCKIOPOOL_SOCKET_TIMEOUT_KEY, DEFAULT_SOCKIOPOOL_SOCKET_TIMEOUT_VALUE);
-    }
-
-    public static boolean getPoolAliveCheck() {
-        return getConf().getBoolean(SOCKIOPOOL_ALIVE_CHECK_KEY, DEFAULT_SOCKIOPOOL_ALIVE_CHECK_VALUE);
-    }
-
-    public static int getPoolMaxIdle() {
-        return getConf().getInt(SOCKIOPOOL_MAX_IDLE_KEY, DEFAULT_SOCKIOPOOL_MAX_IDLE_VALUE);
-    }
-
-    public static int getPoolConnectTimeout() {
-        return getConf().getInt(SOCKIOPOOL_CONNECT_TIMEOUT_KEY, DEFAULT_SOCKIOPOOL_CONNECT_TIMEOUT_VALUE);
-    }
-
-    public static String getPoolMemnamePrefix() {
-        return getConf().getTrimmed(SOCKIOPOOL_MEMNAME_PREFIX_KEY, DEFAULT_SOCKIOPOOL_MEMNAME_PREFIX_VALUE);
-    }
-
-    public static int getPoolGCIntervalMs() {
-        return getConf().getInt(SOCKIOPOOL_GARBAGE_COLLECTION_INTERVAL_MS_KEY, DEFAULT_SOCKIOPOOL_GARBAGE_COLLECTION_INTERVAL_MS_VALUE);
-    }
 }
--- jmemadmin-server/src/main/java/org/exp/jmemadmin/mcserver/MCManager.java
+++ jmemadmin-server/src/main/java/org/exp/jmemadmin/mcserver/MCManager.java
@@ -1,6 +1,6 @@
 package org.exp.jmemadmin.mcserver;
 
-import org.exp.jmemadmin.common.Configs;
+import org.exp.jmemadmin.mcserver.common.ServerConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,17 +24,17 @@ public class MCManager {
         SockIOPool pool = SockIOPool.getInstance(poolName);
         pool.setServers(servers); // 设置memcached服务器地址
         // pool.setWeights(weights); //设置每个memcached服务器权重
-        pool.setFailover(Configs.getPoolFailover()); // 当一个memcached服务器失效的时候是否去连接另一个memcached服务器.
-        pool.setInitConn(Configs.getPoolInitConns()); // 初始化时对每个服务器建立的连接数目
-        pool.setMinConn(Configs.getPoolMinConns()); // 每个服务器建立最小的连接数
-        pool.setMaxConn(Configs.getPoolMaxConns()); // 每个服务器建立最大的连接数
-        pool.setMaintSleep(Configs.getPoolMaintSleep()); // 自查线程周期进行工作，其每次休眠时间
-        pool.setNagle(Configs.getPoolNagle()); // Socket的参数，如果是true，在写数据时不缓冲，立即发送出去。Tcp的规则是在发送一个包之前，包的发送方会等待远程接收方确认已收到上一次发送过来的包；这个方法就可以关闭套接字的缓存——包准备立即发出。
-        pool.setSocketTO(Configs.getPoolSocketTimeout()); // Socket阻塞读取数据的超时时间
-        pool.setAliveCheck(Configs.getPoolAliveCheck()); // 设置是否检查memcached服务器是否失效
-        pool.setMaxIdle(Configs.getPoolMaxIdle()); // 设置最大处理时间
-        pool.setSocketConnectTO(Configs.getPoolConnectTimeout()); // 连接建立时对超时的控制
-        pool.setMaintSleep(Configs.getPoolMaintSleep()); // 设置主线程睡眠时间，每30秒苏醒一次，维持连接池大小
+        pool.setFailover(ServerConfig.getPoolFailover()); // 当一个memcached服务器失效的时候是否去连接另一个memcached服务器.
+        pool.setInitConn(ServerConfig.getPoolInitConns()); // 初始化时对每个服务器建立的连接数目
+        pool.setMinConn(ServerConfig.getPoolMinConns()); // 每个服务器建立最小的连接数
+        pool.setMaxConn(ServerConfig.getPoolMaxConns()); // 每个服务器建立最大的连接数
+        pool.setMaintSleep(ServerConfig.getPoolMaintSleep()); // 自查线程周期进行工作，其每次休眠时间
+        pool.setNagle(ServerConfig.getPoolNagle()); // Socket的参数，如果是true，在写数据时不缓冲，立即发送出去。Tcp的规则是在发送一个包之前，包的发送方会等待远程接收方确认已收到上一次发送过来的包；这个方法就可以关闭套接字的缓存——包准备立即发出。
+        pool.setSocketTO(ServerConfig.getPoolSocketTimeout()); // Socket阻塞读取数据的超时时间
+        pool.setAliveCheck(ServerConfig.getPoolAliveCheck()); // 设置是否检查memcached服务器是否失效
+        pool.setMaxIdle(ServerConfig.getPoolMaxIdle()); // 设置最大处理时间
+        pool.setSocketConnectTO(ServerConfig.getPoolConnectTimeout()); // 连接建立时对超时的控制
+        pool.setMaintSleep(ServerConfig.getPoolMaintSleep()); // 设置主线程睡眠时间，每30秒苏醒一次，维持连接池大小
         pool.initialize(); // 初始化连接池
         LOG.info("****************初始化连接池成功*******************");
         MemCachedClient activeClient = new MemCachedClient(poolName);
