5278081e448894bb0b04b93fc8384b8b4299e2fc
Light-Moon <1424686850@qq.com>
2018-11-06 22:51:52 +0800

    Removed @XmlElement(name = "result") of Response class to repair bug about 'String transform JavaBean'.

--- jmemadmin-common/src/main/java/org/exp/jmemadmin/common/utils/HTTPUtils.java
+++ jmemadmin-common/src/main/java/org/exp/jmemadmin/common/utils/HTTPUtils.java
@@ -24,6 +24,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
 
 /**
  *
@@ -164,7 +165,11 @@ public class HTTPUtils {
         }
         HttpResponse httpResponse = client.execute(post);
         String contentTmp = EntityUtils.toString(httpResponse.getEntity(), Constants.DEFAULT_ENCODING);
-        Response response = JSON.parseObject(contentTmp, Response.class);
+        LOG.info("ContentTmp is [" + contentTmp + "].");
+        Response response = JSON.parseObject(contentTmp, new TypeReference<Response>() {
+        });
+        LOG.info("Response of sendPOSTRequest is [" + response + "].");
         return response;
     }
+
 }
