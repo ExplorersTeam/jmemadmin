8cb570f2ca36b3ebba5891dd645c2ba609010829
Light-Moon <1424686850@qq.com>
2018-11-06 18:25:41 +0800

    Added BAR_DELIMITER configuration item into Constants class.

--- jmemadmin-common/src/main/java/org/exp/jmemadmin/common/Constants.java
+++ jmemadmin-common/src/main/java/org/exp/jmemadmin/common/Constants.java
@@ -22,6 +22,7 @@ public class Constants {
      */
     public static final String SLASH_DELIMITER = "/";
     public static final String COLON_DELIMITER = ":";
+    public static final String BAR_DELIMITER = "-";
     public static final String COMMAND_DELIMITER = " ";
 
     /**
